5aafbb3f95af4248e8c68b58a7aee1f3ad45f164
dclxxxvi <t.aleksandr888@mail.ru>
2022-10-19 11:12:03 +0500

    env configuration, modules, db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import * as path from 'path';
+import { ServeStaticModule } from '../node_modules/@nestjs/serve-static'
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: `.${process.env.NODE_ENV}.env`
+		}),
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: process.env.POSTGRES_HOST,
+			port: Number(process.env.POSTGRES_PORT),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+			models: [],
+			autoLoadModels: true
+		}),
+		ServeStaticModule.forRoot({
+			rootPath: path.resolve(__dirname, 'static')
+		}),
+	],
 })
 export class AppModule {}
